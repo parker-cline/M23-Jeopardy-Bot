e4cf38823df73a8bbb9dea2444510e1e6183b1c2
Parker Cline <parker.cline@minerva.kgi.edu>
2021-04-11 21:08:14 -0400

    random functionality

--- main.py
+++ main.py
@@ -5,8 +5,10 @@
 import logging, random, sqlite3
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, inspect, text
+from sqlalchemy import create_engine, MetaData, Table, text, select, func
+from sqlalchemy.orm import Session
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Enable logging
@@ -15,24 +17,44 @@ logging.basicConfig(
     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# env variables
 import os.path
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "clues.db")
 API_KEY = os.getenv('API_KEY')
 
+# load telegram classes
 updater = Updater(token=API_KEY, use_context=True)
 dispatcher = updater.dispatcher
 
-engine = create_engine('sqlite:///{}'.format(db_path), echo=True, future=True)
+# load sqlalchemy classes
+engine = create_engine('sqlite:///{}'.format(db_path), future=True)
+metadata = MetaData()
+# reflect tables
+clues = Table("clues", metadata, autoload_with=engine)
+airdates = Table("airdates", metadata, autoload_with=engine)
+documents = Table("documents", metadata, autoload_with=engine)
+categories = Table("categories", metadata, autoload_with=engine)
+classifications = Table("classifications", metadata, autoload_with=engine)
 
+
+# TODO: move to unit_test.py
 def test_select(engine):
     with engine.connect() as conn:
-        result = conn.execute(text("select * from clues where id = 10000"))
-        return result.all()
+        stmt = select(clues).where(clues.c.id == 10000)
+        print(conn.execute(stmt).first())
 
-print(test_select(engine))
+def select_random(engine):
+    with engine.connect() as conn:
+        stmt_num_rows = select(func.count('*')).select_from(clues)
+        num_rows = conn.execute(stmt_num_rows).first()[0]
+        random_id = random.randint(1, num_rows)
+        stmt = select(documents).where(documents.c.id == random_id)
+        print(conn.execute(stmt).first())
 
+
+select_random(engine)
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
